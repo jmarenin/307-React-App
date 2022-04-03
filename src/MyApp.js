bc7f30ba4101dd190ad33b32254f6e7e3bb020a8
unknown <jmarenin@calpoly.edu>
2022-04-03 15:38:47 -0700

    Second commit

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Table from './Table'
+import Form from './Form'
 
-const characters = [
+function MyApp() { 
+  const [characters, setCharacters] = useState([
     {
       name: 'Charlie',
       job: 'Janitor',
@@ -18,14 +20,20 @@ const characters = [
       name: 'Dennis',
       job: 'Bartender',
     },
-];
+  ]);
 
-function MyApp() { 
-    return ( 
-      <div className = "container"> 
-        <Table characterData = {characters}/>
-      </div> 
-    );  
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {return i !== index});
+    setCharacters(updated);
+  }
+
+  return (
+    <div className = "container">
+      <Table characterData = {characters} removeCharacter = {removeOneCharacter} />
+      <Form />
+    </div>
+  );
 }   
+
 
 export default MyApp;
